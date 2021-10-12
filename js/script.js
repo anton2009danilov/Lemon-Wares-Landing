32fa31c0a94d0bdf6dfdbfc9047d2646f524e298
anton2009danilov <anton2009danilov@yandex.ru>
2021-10-12 22:47:32 +0300

    add eventListener 'click' on <body>
    move logic from eventListener for burger-btn to eventListener for <body>

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-"use strict";
+'use strict';
 
 window.onload = () => {
-	const burgerBtn = document.getElementById("burger-btn");
-	const burgerMenu = document.getElementById("burger-menu");
+	const burgerMenu = document.getElementById('burger-menu');
 
-	burgerBtn.addEventListener('click', () => {
-		console.log(burgerMenu);
-		burgerMenu.classList.toggle("d-none");
-	})
+	document.body.addEventListener('click', (e) => {
+		console.log(e.target);
+
+		if (e.target.id === 'burger-btn' || e.target.parentNode.id === 'burger-btn') {
+			burgerMenu.classList.toggle('d-none');
+		}
+
+	});
 }
