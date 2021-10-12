d7d8a2c20db10dbb22872cf3dedca287508c6961
anton2009danilov <anton2009danilov@yandex.ru>
2021-10-13 00:44:55 +0300

    add hero section slider

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 
 window.onload = () => {
 	const burgerMenu = document.getElementById('burger-menu');
+	const heroSlider = document.getElementById('hero-slider');
+	let heroSliderWidth = heroSlider.offsetWidth;
+	console.log(heroSliderWidth);
+	heroSlider.scrollLeft = heroSliderWidth;
 
 	document.body.addEventListener('click', (e) => {
 		console.log(e.target);
@@ -10,5 +14,25 @@ window.onload = () => {
 			burgerMenu.classList.toggle('d-none');
 		}
 
+		if (e.target.classList.contains('hero__nav-link')) {
+			const targetLink = e.target;
+			const linkedSlide = document.getElementById(targetLink.dataset.slide);
+			const prevSelectedLink = document.getElementsByClassName('hero__nav-link--selected')[0];
+
+			prevSelectedLink.classList.toggle('hero__nav-link--selected');
+			targetLink.classList.toggle('hero__nav-link--selected');
+
+			console.log(linkedSlide);
+
+			if (!linkedSlide.classList.contains('hero__slide--active')) {
+				const prevSelectidSlide = document.getElementsByClassName('hero__slide--active')[0];
+				prevSelectidSlide.classList.toggle('hero__slide--active');
+				linkedSlide.classList.toggle('hero__slide--active');
+			}
+
+
+		}
+
 	});
+
 }
