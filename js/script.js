ca2061241f30d4ec4db3f87df3f27e5506a6bc05
anton2009danilov <anton2009danilov@yandex.ru>
2021-10-13 23:13:56 +0300

    js code refactoring

--- js/script.js
+++ js/script.js
@@ -1,133 +1,134 @@
 'use strict';
 
 window.onload = () => {
-	const burgerMenu = document.getElementById('burger-menu');
-	const heroSlider = document.getElementById('hero-slider');
-	let heroSliderWidth = heroSlider.offsetWidth;
-	console.log(heroSliderWidth);
-	heroSlider.scrollLeft = heroSliderWidth;
-
 	document.body.addEventListener('click', (e) => {
+		listenToBurgerBtn(e);
+		listenToHeroSlider(e);
+		listenToPaymentToggleBtn(e);
+		listenToPaymentTypeLabel(e);
+		listenToPackageTypeLabel(e);
+	});
+}
 
-		if (e.target.id === 'burger-btn' || e.target.parentNode.id === 'burger-btn') {
-			burgerMenu.classList.toggle('d-none');
+function listenToBurgerBtn(e) {
+	const burgerMenu = document.getElementById('burger-menu');
+
+	if (e.target.id === 'burger-btn' || e.target.parentNode.id === 'burger-btn') {
+		burgerMenu.classList.toggle('d-none');
+	}
+}
+
+function listenToHeroSlider(e) {
+	if (e.target.classList.contains('hero__nav-link')) {
+		const targetLink = e.target;
+		const linkedSlide = document.getElementById(targetLink.dataset.slide);
+		const prevSelectedLink = document.getElementsByClassName('hero__nav-link--selected')[0];
+
+		prevSelectedLink.classList.toggle('hero__nav-link--selected');
+		targetLink.classList.toggle('hero__nav-link--selected');
+
+		if (!linkedSlide.classList.contains('hero__slide--active')) {
+			const prevSelectidSlide = document.getElementsByClassName('hero__slide--active')[0];
+			prevSelectidSlide.classList.toggle('hero__slide--active');
+			linkedSlide.classList.toggle('hero__slide--active');
 		}
 
-		if (e.target.classList.contains('hero__nav-link')) {
-			const targetLink = e.target;
-			const linkedSlide = document.getElementById(targetLink.dataset.slide);
-			const prevSelectedLink = document.getElementsByClassName('hero__nav-link--selected')[0];
+	}
+}
 
-			prevSelectedLink.classList.toggle('hero__nav-link--selected');
-			targetLink.classList.toggle('hero__nav-link--selected');
+function listenToPaymentToggleBtn(e) {
+	if (e.target.id === 'payment-period-toggle-btn') {
+		const paymentToggleBtn = e.target;
+		const monthlyPayment = document.getElementById('monthly-payment');
+		const yearlyPayment = document.getElementById('yearly-payment');
 
-			console.log(linkedSlide);
+		const monthFeeOptions = document.getElementById('month-fee-options');
+		const yearFeeOptions = document.getElementById('year-fee-options');
 
-			if (!linkedSlide.classList.contains('hero__slide--active')) {
-				const prevSelectidSlide = document.getElementsByClassName('hero__slide--active')[0];
-				prevSelectidSlide.classList.toggle('hero__slide--active');
-				linkedSlide.classList.toggle('hero__slide--active');
-			}
+		paymentToggleBtn.classList.toggle('toggle-btn--on');
 
-		}
+		if (paymentToggleBtn.classList.contains('toggle-btn--on')) {
+			monthlyPayment.checked = false;
+			yearlyPayment.checked = true;
 
-		if (e.target.id === 'payment-period-toggle-btn') {
-			const paymentToggleBtn = e.target;
-			const monthlyPayment = document.getElementById('monthly-payment');
-			const yearlyPayment = document.getElementById('yearly-payment');
-
-			const monthFeeOptions = document.getElementById('month-fee-options');
-			const yearFeeOptions = document.getElementById('year-fee-options');
-
-			paymentToggleBtn.classList.toggle('toggle-btn--on');
-
-			if (paymentToggleBtn.classList.contains('toggle-btn--on')) {
-				monthlyPayment.checked = false;
-				yearlyPayment.checked = true;
-
-				monthFeeOptions.classList.remove('pricing__fee-options-container--selected');
-				yearFeeOptions.classList.add('pricing__fee-options-container--selected');
-			} else {
-				monthlyPayment.checked = true;
-				yearlyPayment.checked = false;
-
-				monthFeeOptions.classList.add('pricing__fee-options-container--selected');
-				yearFeeOptions.classList.remove('pricing__fee-options-container--selected');
-			}
-		}
-
-		if (e.target.id === 'monthly-payment-label') {
-			const paymentToggleBtn = document.getElementById('payment-period-toggle-btn');
-			paymentToggleBtn.classList.remove('toggle-btn--on');
-
-			const monthFeeOptions = document.getElementById('month-fee-options');
-			const yearFeeOptions = document.getElementById('year-fee-options');
+			monthFeeOptions.classList.remove('pricing__fee-options-container--selected');
+			yearFeeOptions.classList.add('pricing__fee-options-container--selected');
+		} else {
+			monthlyPayment.checked = true;
+			yearlyPayment.checked = false;
 
 			monthFeeOptions.classList.add('pricing__fee-options-container--selected');
 			yearFeeOptions.classList.remove('pricing__fee-options-container--selected');
 		}
+	}
+}
 
-		if (e.target.id === 'yearly-payment-label') {
-			const paymentToggleBtn = document.getElementById('payment-period-toggle-btn');
-			paymentToggleBtn.classList.add('toggle-btn--on');
+function listenToPaymentTypeLabel(e) {
+	const paymentToggleBtn = document.getElementById('payment-period-toggle-btn');
+	const monthFeeOptions = document.getElementById('month-fee-options');
+	const yearFeeOptions = document.getElementById('year-fee-options');
 
-			const monthFeeOptions = document.getElementById('month-fee-options');
-			const yearFeeOptions = document.getElementById('year-fee-options');
+	if (e.target.id === 'monthly-payment-label') {
+		paymentToggleBtn.classList.remove('toggle-btn--on');
 
-			monthFeeOptions.classList.remove('pricing__fee-options-container--selected');
-			yearFeeOptions.classList.add('pricing__fee-options-container--selected');
+		monthFeeOptions.classList.add('pricing__fee-options-container--selected');
+		yearFeeOptions.classList.remove('pricing__fee-options-container--selected');
+	}
+
+	if (e.target.id === 'yearly-payment-label') {
+		paymentToggleBtn.classList.add('toggle-btn--on');
+
+		monthFeeOptions.classList.remove('pricing__fee-options-container--selected');
+		yearFeeOptions.classList.add('pricing__fee-options-container--selected');
+	}
+}
+
+function listenToPackageTypeLabel(e) {
+
+	const basicPackageLabel = document.getElementById('basic-package-label');
+	const premiumPackageLabel = document.getElementById('premium-package-label');
+
+	const basicPaymentOptions = document.querySelectorAll('.pricing__fee-options[data-pricing-type="basic"]');
+	const premiumPaymentOptions = document.querySelectorAll('.pricing__fee-options[data-pricing-type="premium"]');
+
+	if (e.target.id === 'basic-package-label') {
+		const basicPackageLabel = e.target;
+
+		if (!basicPackageLabel.classList.contains('pricing__payment-type-label--selected')) {
+			basicPackageLabel.classList.add('pricing__payment-type-label--selected');
+			premiumPackageLabel.classList.remove('pricing__payment-type-label--selected');
 		}
 
-		if (e.target.id === 'basic-package-label') {
-			const basicPackageLabel = e.target;
-			const premiumPackageLabel = document.getElementById('premium-package-label');
-
-			if (!basicPackageLabel.classList.contains('pricing__payment-type-label--selected')) {
-				basicPackageLabel.classList.add('pricing__payment-type-label--selected');
-				premiumPackageLabel.classList.remove('pricing__payment-type-label--selected');
+		basicPaymentOptions.forEach(el => {
+			if (!el.classList.contains('pricing__fee-options--selected')) {
+				el.classList.add('pricing__fee-options--selected');
 			}
+		});
 
-			const basicPaymentOptions = document.querySelectorAll('.pricing__fee-options[data-pricing-type="basic"]');
-			const premiumPaymentOptions = document.querySelectorAll('.pricing__fee-options[data-pricing-type="premium"]');
-
-			basicPaymentOptions.forEach(el => {
-				if (!el.classList.contains('pricing__fee-options--selected')) {
-					el.classList.add('pricing__fee-options--selected');
-				}
-			});
-
-			premiumPaymentOptions.forEach(el => {
-				if (el.classList.contains('pricing__fee-options--selected')) {
-					el.classList.remove('pricing__fee-options--selected');
-				}
-			});
-		}
-
-		if (e.target.id === 'premium-package-label') {
-			const premiumPackageLabel = e.target;
-			const basicPackageLabel = document.getElementById('basic-package-label');
-
-			if (!premiumPackageLabel.classList.contains('pricing__payment-type-label--selected')) {
-				basicPackageLabel.classList.remove('pricing__payment-type-label--selected');
-				premiumPackageLabel.classList.add('pricing__payment-type-label--selected');
+		premiumPaymentOptions.forEach(el => {
+			if (el.classList.contains('pricing__fee-options--selected')) {
+				el.classList.remove('pricing__fee-options--selected');
 			}
+		});
+	}
 
-			const basicPaymentOptions = document.querySelectorAll('.pricing__fee-options[data-pricing-type="basic"]');
-			const premiumPaymentOptions = document.querySelectorAll('.pricing__fee-options[data-pricing-type="premium"]');
+	if (e.target.id === 'premium-package-label') {
 
-			basicPaymentOptions.forEach(el => {
-				if (el.classList.contains('pricing__fee-options--selected')) {
-					el.classList.remove('pricing__fee-options--selected');
-				}
-			});
-
-			premiumPaymentOptions.forEach(el => {
-				if (!el.classList.contains('pricing__fee-options--selected')) {
-					el.classList.add('pricing__fee-options--selected');
-				}
-			});
+		if (!premiumPackageLabel.classList.contains('pricing__payment-type-label--selected')) {
+			basicPackageLabel.classList.remove('pricing__payment-type-label--selected');
+			premiumPackageLabel.classList.add('pricing__payment-type-label--selected');
 		}
 
-	});
+		basicPaymentOptions.forEach(el => {
+			if (el.classList.contains('pricing__fee-options--selected')) {
+				el.classList.remove('pricing__fee-options--selected');
+			}
+		});
 
+		premiumPaymentOptions.forEach(el => {
+			if (!el.classList.contains('pricing__fee-options--selected')) {
+				el.classList.add('pricing__fee-options--selected');
+			}
+		});
+	}
 }
